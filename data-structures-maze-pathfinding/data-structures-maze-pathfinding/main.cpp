b07c4ac63db5bce5fd286171541b968b526995b1
Alexander Cochrane <acochrane@almightycorp.com>
2017-02-08 14:01:39 -0400

    added main file

---
+++ data-structures-maze-pathfinding/data-structures-maze-pathfinding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data-structures-maze-pathfinding
+//
+//  Created by Alexander Cochrane on 2017-02-08.
+//  Copyright © 2017 Alexander Cochrane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
